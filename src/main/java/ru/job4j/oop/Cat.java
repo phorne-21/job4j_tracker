96d52e7fd868c575f20bf9acbcecd5be2e81c615
phorne-21 <lwrow@rambler.ru>
2021-07-12 21:17:39 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -3,13 +3,19 @@ package ru.job4j.oop;
 public class Cat {
 
     private String food;
+    private String name;
 
     public void show() {
+        System.out.println(this.name);
         System.out.println(this.food);
     }
 
     public void eat(String meat) {
         this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
     }
 
     public static void main(String[] args) {
@@ -21,6 +27,5 @@ public class Cat {
         Cat black = new Cat();
         black.eat("fish");
         black.show();
-
     }
 }
