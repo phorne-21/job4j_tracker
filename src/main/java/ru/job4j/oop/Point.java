bd05e82da188d0fa3d54fa4c5ef15b2b95e1cfa2
phorne-21 <lwrow@rambler.ru>
2021-07-14 09:40:42 +0300

    2.2. Point

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -5,7 +5,9 @@ import static java.lang.Math.pow;
 
 public class Point {
     private int x;
+
     private  int y;
+
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
@@ -13,12 +15,5 @@ public class Point {
 
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
-    }
-
-    public static void main(String[] args) {
-        Point a = new Point(3, 2);
-        Point b = new Point(7, 6);
-        double distance = a.distance(b);
-        System.out.println(distance);
     }
 }
