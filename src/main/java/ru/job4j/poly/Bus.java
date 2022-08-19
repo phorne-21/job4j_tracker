abdc532820de13badc0b8b9ce3b4658f8f962b75
Filippova Ekaterina <lwrow220816@gmail.com>
2022-08-19 14:00:20 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void drive() {
+        return;
+    }
+
+    @Override
+    public void passengers(int num) {
+        return;
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        return 0;
+    }
+}
